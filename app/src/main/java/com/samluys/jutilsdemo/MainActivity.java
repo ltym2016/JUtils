5074d51bfdc2f5208be072a25067fcecc412e672
luyousheng <luyousheng>
2020-01-16 14:41:50 +0800

    状态栏工具类添加

--- app/src/main/java/com/samluys/jutilsdemo/MainActivity.java
+++ app/src/main/java/com/samluys/jutilsdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.samluys.jutils.ScreenUtils;
 import com.samluys.jutils.ToastUtils;
+import com.samluys.statusbar.StatusBarUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ToastUtils.showLong("屏幕宽度 " + ScreenUtils.getScreenWidth(this));
+
+        StatusBarUtils.transparencyBar(this);
     }
 }
